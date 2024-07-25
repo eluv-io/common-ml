b3367765c34edbcee4c76ca328df9f84898ee2cc
elv-nickB <nick.buoncristiani@eluv.io>
2024-07-25 18:56:11 +0000

    removed version reqs on dependencies

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ setup(
     install_requires=[
         'opencv-python',
         'ffmpeg-python==0.2.0',
-        'ujson==5.7.0',
-        'loguru==0.5.2',
-        'schema==0.7.5',
-        'PyYAML==6.0.1',
-        'marshmallow==3.19.0',
+        'ujson',
+        'loguru',
+        'schema',
+        'PyYAML',
+        'marshmallow',
     ]
 )
