8f2c80de00e73c6ca81c1465585c853969f0f4d3
elv-nickB <nick.buoncristiani@eluv.io>
2024-11-27 17:40:15 +0000

    Added data abc

---
+++ common_ml/types.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, asdict
+
+@dataclass
+class Data(ABC):
+    """
+    This class is useful for converting data to and from a dictionary or url request. Often explicit type conversion and verification is 
+    necessary and this class provides a common interface for that.
+
+    The `from_dict` method is a class method that should be implemented by subclasses, it should take a dictionary and return an instance of the class 
+    while raising an exception if the dictionary is not valid.
+    """
+    @abstractmethod
+    @staticmethod
+    def from_dict(data: dict) -> 'Data':
+        pass
+
+    def to_dict(self) -> dict:
+        return asdict(self)
