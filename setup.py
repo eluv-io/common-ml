00b9ff773c7d35ce3dff3ada1d511d02cfe9fc98
elv-nickB <nick.buoncristiani@eluv.io>
2025-01-22 18:26:01 +0000

    switched to find_packages in setup.py for submodules

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="common-ml",
     version="0.1",
-    packages=['common_ml'],
+    packages=find_packages(),
     install_requires=[
         'ffmpeg-python==0.2.0',
         'numpy',
