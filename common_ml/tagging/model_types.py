46ef0859d4e5f8ab68a815723db19d43e8f98bac
Nicholas Buoncristiani <nick.buoncristiani@eluv.io>
2026-03-12 21:32:07 +0000

    made model types runtime checkable

--- common_ml/tagging/model_types.py
+++ common_ml/tagging/model_types.py
@@ -1,14 +1,17 @@
-from typing import List, Protocol
+from typing import List, Protocol, runtime_checkable
 
 import numpy as np
 
 from common_ml.tagging.messages import Tag, FrameTag
 
+@runtime_checkable
 class VideoModel(Protocol):
-    def tag(self, fpath: str) -> List[Tag]: ...
-
+    def tag_video(self, fpath: str) -> List[Tag]: ...
+    
+@runtime_checkable
 class FrameModel(Protocol):
-    def tag(self, img: np.ndarray) -> List[FrameTag]: ...  # img: (H, W, 3)
+    def tag_frame(self, img: np.ndarray) -> List[FrameTag]: ...  # img: (H, W, 3)
 
+@runtime_checkable
 class BatchFrameModel(Protocol):
-    def tag(self, imgs: np.ndarray) -> List[List[FrameTag]]: ...  # imgs: (N, H, W, 3)
+    def tag_frames(self, imgs: np.ndarray) -> List[List[FrameTag]]: ...  # imgs: (N, H, W, 3)
