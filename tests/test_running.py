a24ed23b52f35df007ca0adb77c2e61a7fc8ee2c
Nicholas Buoncristiani <nick.buoncristiani@eluv.io>
2026-03-16 21:09:19 +0000

    added test for allow_single_frame=False option

--- tests/test_running.py
+++ tests/test_running.py
@@ -72,3 +72,18 @@ def test_default_tag_frame_model_videos(frame_model: FrameModel, test_videos: Li
     for line in video_lines:
         tag = from_dict(Tag, json.loads(line))
         assert tag.end_time > tag.start_time
+
+def test_default_tag_frame_model_videos_single_false(frame_model: FrameModel, test_videos: List[str], test_folder: str):
+    output_path = os.path.join(test_folder, "out.jsonl")
+    default_tag_frame_model(frame_model, test_videos, output_path, fps=1, allow_single_frame=False)
+
+    with open(output_path, "r") as f:
+        lines = f.readlines()
+    
+    frame_lines = [l for l in lines if "frame_idx" in l]
+    video_lines = [l for l in lines if "frame_idx" not in l]
+
+    for line in video_lines:
+        tag = from_dict(Tag, json.loads(line))
+        # we shouldn't have single frame video tags. 
+        assert tag.end_time > tag.start_time + 1000
