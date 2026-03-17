d6f5dd15ceb27cd309c79c87628fc071a1d0f98c
Nicholas Buoncristiani <nick.buoncristiani@eluv.io>
2026-03-17 23:53:48 +0000

    added docstrings for frame model input format

--- common_ml/tagging/models/frame_based.py
+++ common_ml/tagging/models/frame_based.py
@@ -8,12 +8,24 @@ from common_ml.tagging.models.tag_types import FrameTag
     
 class FrameModel(ABC):
     @abstractmethod
-    def tag_frame(self, img: np.ndarray) -> List[FrameTag]: pass  # img: (H, W, 3)
+    def tag_frame(self, img: np.ndarray) -> List[FrameTag]:
+        """
+        Parameters
+        ----------
+        img : np.ndarray, shape (H, W, 3), dtype uint8
+            RGB image.
+        """
 
 
 class BatchFrameModel(ABC):
     @abstractmethod
-    def tag_frames(self, imgs: np.ndarray) -> List[List[FrameTag]]: pass  # imgs: (N, H, W, 3)
+    def tag_frames(self, imgs: np.ndarray) -> List[List[FrameTag]]:
+        """
+        Parameters
+        ----------
+        imgs : np.ndarray, shape (N, H, W, 3), dtype uint8
+            Batch of RGB images.
+        """
 
     @staticmethod
     def from_frame_model(model: 'FrameModel') -> 'BatchFrameModel':
