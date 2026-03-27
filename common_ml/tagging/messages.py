fb1e8c19195764c4a94f4a2729e86b32b5a3e3dc
Nicholas Buoncristiani <nick.buoncristiani@eluv.io>
2026-03-27 18:40:10 +0000

    moved Tag to messages.py and got rid of Message wrapper types

--- common_ml/tagging/messages.py
+++ common_ml/tagging/messages.py
@@ -1,16 +1,29 @@
-from typing import List, Literal, Optional, Dict, Protocol
+from typing import Literal, Optional, Dict
 from dataclasses import dataclass
-
-from common_ml.tagging.models.tag_types import Tag
 
 class Message: ...
 
+@dataclass(frozen=True)
+class FrameInfo:
+    frame_idx: int
+    box: Dict[str, float]
+
+@dataclass(frozen=True)
+class Tag(Message):
+    start_time: int
+    end_time: int
+    tag: str
+    source_media: str
+    track: str = ""
+    additional_info: Optional[Dict] = None
+    frame_info: Optional[FrameInfo] = None
+
 @dataclass
-class Progress:
+class Progress(Message):
     source_media: str
 
 @dataclass
-class Error:
+class Error(Message):
     message: str
     source_media: Optional[str] = None
 
