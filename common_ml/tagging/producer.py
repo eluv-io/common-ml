3c89890c80ee80db020dc578a22198453368416e
elv-joeA <joe.anakata@eluv.io>
2026-06-30 18:46:43 +0000

    this is the implementation of helper for processors

--- common_ml/tagging/producer.py
+++ common_ml/tagging/producer.py
@@ -6,7 +6,9 @@ from typing import Union, Iterator
 from common_ml.tagging.messages import *
 from common_ml.tagging.models.frame_based import *
 from common_ml.tagging.models.av import AVModel
+from common_ml.tagging.models.processor import TagProcessor
 from common_ml.tagging.file_tagger import FileTagger
+from common_ml.tagging.processor_helper import TagProcessorAdapterLogic
 
 class TagMessageProducer(ABC):
     @abstractmethod
@@ -21,7 +23,7 @@ class TagMessageProducer(ABC):
     
     @staticmethod
     def from_file_tagger(file_tagger: FileTagger) -> "TagMessageProducer":
-        class NewTagMessageProducer(TagMessageProducer):
+        class FileTaggerToTagMessageProducerAdapter(TagMessageProducer):
             def produce(self, files: List[str]) -> Iterator[Message]:
                 for fname in files:
                     tags = file_tagger.tag(fname)
@@ -31,19 +33,35 @@ class TagMessageProducer(ABC):
 
                     yield Progress(source_media=fname)
 
-        return NewTagMessageProducer()
+        return FileTaggerToTagMessageProducerAdapter()
+
+    @staticmethod
+    def from_tag_processor(tag_processor: TagProcessor) -> "TagMessageProducer":
+        class TagProcessorToTagMessageProducerAdapter(TagMessageProducer):
+            ## this is written like this to avoid circular dependency
+            ## (and avoiding all the tag processor -> producer code being here in this class, since it's a lot)
+            def __init__(self, tag_processor: TagProcessor):
+                self.tag_processor_adapter_logic = TagProcessorAdapterLogic(tag_processor)
+
+            def produce(self, files: List[str]) -> Iterator[Message]:
+                return self.tag_processor_adapter_logic.produce(files)
+
+            def on_completion(self) -> Iterator[Message]:
+                return self.tag_processor_adapter_logic.on_completion()
+
+        return TagProcessorToTagMessageProducerAdapter(tag_processor)
     
     @staticmethod
     def from_model(
-        model: Union[AVModel, FrameModel, BatchFrameModel], 
+        model: Union[AVModel, FrameModel, BatchFrameModel, TagProcessor],
         fps: float=1.0, 
         allow_single_frame: bool=True
     ) -> 'TagMessageProducer':
         if isinstance(model, AVModel):
-            file_tagger = FileTagger.from_video_model(model)
+            return TagMessageProducer.from_file_tagger(FileTagger.from_video_model(model))
         elif isinstance(model, (FrameModel, BatchFrameModel)):
-            file_tagger = FileTagger.from_frame_model(model, fps, allow_single_frame)
+            return TagMessageProducer.from_file_tagger(FileTagger.from_frame_model(model, fps, allow_single_frame))
+        elif isinstance(model, TagProcessor):
+            return TagMessageProducer.from_tag_processor(model)
         else:
-            raise ValueError("Model must be either AVModel, FrameModel, or BatchFrameModel")
-
-        return TagMessageProducer.from_file_tagger(file_tagger)
+            raise ValueError("Model must be either AVModel, FrameModel, BatchFrameModel, or TagProcessor")
