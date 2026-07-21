761fe18339ea1b91c5d8e59bec8c79344468ed0d
elv-grace <grace.wang-intern@eluv.io>
2026-07-21 20:55:15 +0000

    [v4] Updated FrameModel to support vectors via BaseFrameTag

--- common_ml/tagging/models/frame_based.py
+++ common_ml/tagging/models/frame_based.py
@@ -4,11 +4,11 @@ from abc import ABC, abstractmethod
 from typing import List
 import numpy as np
 
-from common_ml.tagging.models.tag_types import FrameTag
-    
+from common_ml.tagging.models.tag_types import BaseFrameTag
+
 class FrameModel(ABC):
     @abstractmethod
-    def tag_frame(self, img: np.ndarray) -> List[FrameTag]:
+    def tag_frame(self, img: np.ndarray) -> List[BaseFrameTag]:
         """
         Parameters
         ----------
@@ -19,7 +19,7 @@ class FrameModel(ABC):
 
 class BatchFrameModel(ABC):
     @abstractmethod
-    def tag_frames(self, imgs: np.ndarray) -> List[List[FrameTag]]:
+    def tag_frames(self, imgs: np.ndarray) -> List[List[BaseFrameTag]]:
         """
         Parameters
         ----------
@@ -30,6 +30,6 @@ class BatchFrameModel(ABC):
     @staticmethod
     def from_frame_model(model: 'FrameModel') -> 'BatchFrameModel':
         class NewModel(BatchFrameModel):
-            def tag_frames(self, imgs: np.ndarray) -> List[List[FrameTag]]:
+            def tag_frames(self, imgs: np.ndarray) -> List[List[BaseFrameTag]]:
                 return [model.tag_frame(img) for img in imgs]
         return NewModel()
